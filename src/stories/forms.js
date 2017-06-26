c9ef4ee7a79bc3b9546ebc79c9f2860ee86ace8d
Rhys Powell <rhys@rpowell.me>
2017-06-26 20:28:00 +1000

    updated storybook

--- src/stories/forms.js
+++ src/stories/forms.js
@@ -1,77 +1,39 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import { action } from "@storybook/addon-actions"
-import {
-  TextField,
-  Form,
-  Label,
-  FormGroup,
-  FormActions
-} from "../components/forms"
-import { Button, SubmitButton } from "../components/Button"
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { TextField, Form, Label, FormActions } from '../components/forms'
+import { Button, SubmitButton } from '../components/Button'
 
-storiesOf("TextField", module)
-  .add("with the default style", () => (
-    <TextField onChange={action("change")} />
-  ))
-  .add("with the block style", () => (
-    <TextField onChange={action("change")} block />
-  ))
-  .add("with value", () => (
-    <TextField onChange={action("change")} value="A value" />
-  ))
-  .add("with placeholder", () => (
-    <TextField onChange={action("change")} placeholder="A placeholder" />
-  ))
+storiesOf('TextField', module)
+  .add('with the default style', () =>
+    <TextField onChange={action('change')} />
+  )
+  .add('with the block style', () =>
+    <TextField onChange={action('change')} block />
+  )
+  .add('with value', () =>
+    <TextField onChange={action('change')} value="A value" />
+  )
+  .add('with placeholder', () =>
+    <TextField onChange={action('change')} placeholder="A placeholder" />
+  )
 
-storiesOf("Form")
-  .add("with the default appearance", () => (
-    <Form
-      onSubmit={e => {
-        e.preventDefault()
-        action("submitted")(e)
-      }}
-    >
-      <FormGroup>
-        <Label htmlFor="one">Text Field</Label>
-        <TextField id="ome" placeholder="Placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label htmlFor="twp">Text Field</Label>
-        <TextField id="twp" placeholder="Placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label htmlFor="three">Text Field</Label>
-        <TextField id="three" placeholder="Placeholder" />
-      </FormGroup>
-      <FormActions>
-        <Button>Cancel</Button>
-        <SubmitButton primary value="Submit" />
-      </FormActions>
-    </Form>
-  ))
-  .add("with the inline style", () => (
-    <Form
-      onSubmit={e => {
-        e.preventDefault()
-        action("submitted")(e)
-      }}
-    >
-      <FormGroup inline>
-        <Label htmlFor="one">Text Field</Label>
-        <TextField id="ome" placeholder="Placeholder" />
-      </FormGroup>
-      <FormGroup inline>
-        <Label htmlFor="twp">Text Field With a Long Label</Label>
-        <TextField id="twp" placeholder="Placeholder" />
-      </FormGroup>
-      <FormGroup inline>
-        <Label htmlFor="three">Text Field</Label>
-        <TextField id="three" placeholder="Placeholder" />
-      </FormGroup>
-      <FormActions>
-        <Button>Cancel</Button>
-        <SubmitButton primary value="Submit" />
-      </FormActions>
-    </Form>
-  ))
+storiesOf('Form').add('with the default appearance', () =>
+  <Form
+    onSubmit={e => {
+      e.preventDefault()
+      action('submitted')(e)
+    }}
+  >
+    <Label htmlFor="one">Text Field</Label>
+    <TextField id="ome" placeholder="Placeholder" />
+    <Label htmlFor="twp">Text Field</Label>
+    <TextField id="twp" placeholder="Placeholder" />
+    <Label htmlFor="three">Text Field</Label>
+    <TextField id="three" placeholder="Placeholder" />
+    <FormActions>
+      <Button>Cancel</Button>
+      <SubmitButton primary value="Submit" />
+    </FormActions>
+  </Form>
+)
